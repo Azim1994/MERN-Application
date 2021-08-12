41390bf56a6743d62273c34ddd69de7dd77a974a
Azim Usmanov <azim.usmanov1994@gmail.com>
2021-08-12 16:22:48 +0500

    MERN Application

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,9 @@
+//for having less erors in the actions folder and reducers
+export const CREATE = 'CREATE';
+export const UPDATE = 'UPDATE';
+export const DELETE = 'DELETE';
+export const FETCH_ALL = 'FETCH ALL';
+export const LIKE = 'LIKE';
+
+export const AUTH = 'AUTH';
+export const LOGOUT = 'LOGOUT';
